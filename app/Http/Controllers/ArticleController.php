abafaf663274fec594e7f631d841b3b23e42f864
John Morris <johnmorris4@bellsouth.net>
2017-12-30 04:43:31 -0500

    finished Api

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Article;
+use App\Http\Resources\Article as ArticleResource;
 
 class ArticleController extends Controller
 {
@@ -13,28 +16,23 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        //
+        $articles = Article::paginate(15);
+        //return collection of articles as a resource
+        return ArticleResource::collection($articles); 
     }
 
-    // /**
-    //  * Show the form for creating a new resource.
-    //  *
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // public function create()
-    // {
-    //     //
-    // }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    
     public function store(Request $request)
     {
-        //
+        $article = $request->isMethod('put') ? Article::findOrFail($request->article_id) : new Article;
+
+        $article->id = $request->input('article_id');
+        $article->title = $request->input('title');
+        $article->body = $request->input('body');
+        
+        if($article->save()) {
+            return new ArticleResource($article);
+        }
     }
 
     /**
@@ -45,31 +43,10 @@ class ArticleController extends Controller
      */
     public function show($id)
     {
-        //
+        //Get Single Resource
+        $article = Article::findOrFail($id);
+        return new ArticleResource($article);
     }
-
-    // /**
-    //  * Show the form for editing the specified resource.
-    //  *
-    //  * @param  int  $id
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // public function edit($id)
-    // {
-    //     //
-    // }
-
-    // /**
-    //  * Update the specified resource in storage.
-    //  *
-    //  * @param  \Illuminate\Http\Request  $request
-    //  * @param  int  $id
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // public function update(Request $request, $id)
-    // {
-    //     //
-    // }
 
     /**
      * Remove the specified resource from storage.
@@ -80,5 +57,10 @@ class ArticleController extends Controller
     public function destroy($id)
     {
         //
+        $article = Article::findOrFail($id);
+        if($article->delete()){
+            return new ArticleResource($article);
+        }
+        
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,14 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//List Articles
+Route::get('articles', 'ArticleController@index');
+//List Single Article
+Route::get('article/{id}', 'ArticleController@show');
+//Create
+Route::post('article', 'ArticleController@store');
+//Update
+Route::put('article', 'ArticleController@store');
+//Destroy
+Route::delete('article/{id}', 'ArticleController@destroy');
